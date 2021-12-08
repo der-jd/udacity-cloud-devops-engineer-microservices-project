da6cb7b01abab5c9ffe46cb4000c91c47b1133c7
der-jd <ec2-user@ip-172-31-9-16.eu-central-1.compute.internal>
2021-12-08 12:27:46 +0000

    task4: upload docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=judt/udacity-cloud-devops-engineer-microservices
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+echo "Login to DockerHub..."
+docker login --username judt
+docker tag udacity-project $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
